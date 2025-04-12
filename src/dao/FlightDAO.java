0da55cf0a07e867ba1d88a2796dbd5a6f4343926
seyran7 <brsr888@gmail.com>
2025-04-12 10:23:39 +0400

    Added a part of the Flight

--- src/dao/FlightDAO.java
+++ src/dao/FlightDAO.java
@@ -1,4 +1,12 @@
 package dao;
 
+import entity.Flight;
+
+import java.util.List;
+
 public interface FlightDAO {
+    void addFlight(Flight flight);
+    void deleteFlight(int id);
+    Flight getFlightById(int id);
+    List<Flight>getAllFlight();
 }
--- src/dao/FlightDaoImpl.java
+++ src/dao/FlightDaoImpl.java
@@ -1,4 +1,37 @@
 package dao;
 
+import entity.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FlightDaoImpl implements FlightDAO {
+    private List<Flight>flights=new ArrayList<>();
+
+    @Override
+    public void addFlight(Flight flight) {
+        flights.add(flight);
+        System.out.println("New flight added: " + flight);
+
+    }
+
+    @Override
+    public void deleteFlight(int id) {
+        flights.removeIf(f->f.getId()==id);
+        System.out.println("Flight where id=: "+ id +"deleted");
+
+    }
+
+    @Override
+    public Flight getFlightById(int id) {
+        return flights.stream()
+                .filter(f->f.getId()==id)
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Flight> getAllFlight() {
+        return new ArrayList<>(flights);
+    }
 }
